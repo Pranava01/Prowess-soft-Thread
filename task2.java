ddb364f294eaa4da427bb1437f8662913ab5ee69
Pranava01 <77974071+Pranava01@users.noreply.github.com>
2022-03-20 09:46:51 +0530

    Update task2.java

--- task2.java
+++ task2.java
@@ -1,10 +1,11 @@
-public class task2 extends Thread
+public class task2 implements Runnable
 { public void run()
   { System.out.println("Thread name is : "+Thread.currentThread().getName());
   }
   public static void main(String args[])
   {
-   task1 t2 = new task2();
-   t2.start();
+   task2 t2 = new task2();
+   Thread th1 = new Thread(t2, "First Thread");
+   th1.start();
    }
  }
