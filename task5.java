efc606420f10c75cb7baa2c02ea0c40e3dba69aa
Pranava01 <77974071+Pranava01@users.noreply.github.com>
2022-03-20 10:22:29 +0530

    Update task5.java

--- task5.java
+++ task5.java
@@ -1,6 +1,6 @@
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-public class SimpleThreadPool 
+public class task5 
 {    public static void main(String[] args)
  {   ExecutorService executor = Executors.newFixedThreadPool(5);
         for (int i = 1; i <=10; i++) 
